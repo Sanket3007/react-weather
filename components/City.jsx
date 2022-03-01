0cc324bdda2e8eeca1833741b65cf751e66ecf40
Sanket3007 <karanjesanket1980@gmail.com>
2022-03-01 17:06:32 +0530

    Search buttons returns the weatherInfo component

--- components/City.jsx
+++ components/City.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import './city.css';
 
-const City = () => {
+const City = (props) => {
+
+    // use props
+    const {updateCity, fetchWeather} = props;
+
   return (
     <>
         <img className='city-img' src="/icons/perfect-day.svg" alt="" />
-
         <p className="city-title">Find Weather of your city</p>
-        <div className="city-search">
-            <input type="text" placeholder="City" />
-            <button>Search</button>
-        </div>
+
+        {/*  */}
+        <form className="city-search" onSubmit={fetchWeather}>
+            <input type="text" onChange={(e) => updateCity(e.target.value)} placeholder="City" />
+            <button type={"submit"}>Search</button>
+        </form>
     </>
   )
 }
--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
-import City from '../components/City'
+import { useState } from 'react'
+import axios from "axios";
+import City from '../components/City';
 import WeatherInfo from '../components/WeatherInfo'
 import './App.css'
 
 function App() {
 
+  // define states
+  const [city, updateCity] = useState();
+  const [weather, updateWeather] = useState();
+
+  // fetch the API
+  const fetchWeather = async (e) => {
+    e.preventDefault();
+    const response = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=fe4feefa8543e06d4f3c66d92c61b69c`,
+    );
+    updateWeather(response.data);
+  }
+
   return (
     <div className='main'>
       <p className='app-label'>React Weather</p>
-      <City/>
-      {/* <WeatherInfo/> */}
+      {city && weather ? (<WeatherInfo/>) : ( <City updateCity={updateCity} fetchWeather={fetchWeather}/>)}
     </div>
   )
 }
